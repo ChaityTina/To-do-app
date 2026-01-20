a022f3d7f9fb9ef217c7836b418d8234f2fef9b8
Chaity_Barua <108208797+ChaityTina@users.noreply.github.com>
2026-01-21 02:31:24 +0600

    Add files via upload

--- todo_model.dart
+++ todo_model.dart
@@ -1,1 +1,17 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class Todo {
+  String title;
+  String place;
+  DateTime date;
+  TimeOfDay time;
+  bool isDone;
+
+  Todo({
+    required this.title,
+    required this.place,
+    required this.date,
+    required this.time,
+    this.isDone = false,
+  });
+}
