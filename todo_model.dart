fd6c55ab6420e129e87a01ec6e47855be5258e65
Chaity_Barua <108208797+ChaityTina@users.noreply.github.com>
2026-01-27 00:30:53 +0600

    Add files via upload

--- todo_model.dart
+++ todo_model.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+enum Priority { low, medium, high }
+
 class Todo {
   String title;
   String place;
   DateTime date;
   TimeOfDay time;
   bool isDone;
+  Priority priority;
 
   Todo({
     required this.title,
@@ -13,5 +16,6 @@ class Todo {
     required this.date,
     required this.time,
     this.isDone = false,
+    this.priority = Priority.medium,
   });
 }
