41baa813a4ffa7057702cd9ccd567339bf6085d7
Chaity_Barua <108208797+ChaityTina@users.noreply.github.com>
2026-02-06 12:51:21 +0600

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do_application/screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('todos');
+
   runApp(const MyApp());
 }
 
@@ -12,8 +17,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Todo Application',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'To Do Application',
+
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+
       home: const HomeScreen(),
     );
   }
