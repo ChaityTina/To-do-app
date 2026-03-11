ea74506ac642ccd80bb416072e895d8d5bb9ca9c
Chaity_Barua <108208797+ChaityTina@users.noreply.github.com>
2026-03-11 23:42:00 +0600

    Add files via upload

--- todo_model.dart
+++ todo_model.dart
@@ -1,21 +1,33 @@
-import 'package:flutter/material.dart';
-
 enum Priority { low, medium, high }
 
 class Todo {
   String title;
   String place;
-  DateTime date;
-  TimeOfDay time;
-  bool isDone;
+  DateTime dateTime;
   Priority priority;
+  bool isDone;
 
   Todo({
     required this.title,
     required this.place,
-    required this.date,
-    required this.time,
+    required this.dateTime,
+    required this.priority,
     this.isDone = false,
-    this.priority = Priority.medium,
   });
+
+  Map<String, dynamic> toMap() => {
+    'title': title,
+    'place': place,
+    'dateTime': dateTime.toIso8601String(),
+    'priority': priority.index,
+    'isDone': isDone,
+  };
+
+  factory Todo.fromMap(Map map) => Todo(
+    title: map['title'],
+    place: map['place'],
+    dateTime: DateTime.parse(map['dateTime']),
+    priority: Priority.values[map['priority']],
+    isDone: map['isDone'],
+  );
 }
